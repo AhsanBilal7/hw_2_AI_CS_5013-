0e89c6b60e8b1c66df65f0b4b671710b96911c0a
Ahsan Bilal <bilal.ahsan7705@gmail.com>
2024-09-26 15:36:27 -0500

    finalize local search

--- hw2_local.py
+++ hw2_local.py
@@ -4,71 +4,58 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 import warnings
-warnings.filterwarnings("ignore")
-
-df = pd.read_csv('./CreditCard.csv')
-
-df['CarOwner'] = df['CarOwner'].map({'Y': 1, 'N': 0})
-df['PropertyOwner'] = df['PropertyOwner'].map({'Y': 1, 'N': 0})
-df['Gender'] = df['Gender'].map({'M': 1, 'F': 0})
-df.head()
-
-
-
-df.duplicated().sum()
-df.columns
-
-
-df.info()
-
-
-cols = ['Gender', 'CarOwner', 'PropertyOwner']
-
-# Set up the figure and axes
-
-fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(18, 6))
-
-# Plot univariate distributions for each column
-for i, col in enumerate(cols):
-    sns.countplot(data=df , x=col, ax=axes[i])
-    axes[i].set_title(f'Distribution of {col}' , fontsize=18 )
-    axes[i].set_ylabel('Count')
-    axes[i].tick_params(axis='x')
-    axes[i].grid(True)
-
-plt.tight_layout()
-# plt.show()
-
-
-
-features=['CreditApprove',	'Gender',	'CarOwner',	'PropertyOwner',	'#Children',	'WorkPhone',	'Email_ID']
-
-plt.figure(figsize = (10,6))
-sns.heatmap(df[features].corr(), annot=True , cmap='coolwarm')
-plt.title("Correlation Heatmap")
-# plt.show()
-
-
-
 # example of hill climbing the test set for a classification task
 from random import randint
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from matplotlib import pyplot
-
 import numpy as np
 import pandas as pd
-
 # Visualization libaraires
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+# example of hill climbing the test set for a classification task
+from random import randint
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from matplotlib import pyplot
+import random
 # ignore warning
 import warnings
 warnings.filterwarnings("ignore")
+
+
+df = pd.read_csv('./CreditCard.csv')
+df['CarOwner'] = df['CarOwner'].map({'Y': 1, 'N': 0})
+df['PropertyOwner'] = df['PropertyOwner'].map({'Y': 1, 'N': 0})
+df['Gender'] = df['Gender'].map({'M': 1, 'F': 0})
+df.duplicated().sum()
+df.columns
+df.info()
+cols = ['Gender', 'CarOwner', 'PropertyOwner']
+# Set up the figure and axes
+
+
+def column_distribution():
+    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(18, 6))
+    # Plot univariate distributions for each column
+    for i, col in enumerate(cols):
+        sns.countplot(data=df , x=col, ax=axes[i])
+        axes[i].set_title(f'Distribution of {col}' , fontsize=18 )
+        axes[i].set_ylabel('Count')
+        axes[i].tick_params(axis='x')
+        axes[i].grid(True)
+    plt.tight_layout()
+
+
+def feature_corrlelation():
+    features=['CreditApprove',	'Gender',	'CarOwner',	'PropertyOwner',	'#Children',	'WorkPhone',	'Email_ID']
+    plt.figure(figsize = (10,6))
+    sns.heatmap(df[features].corr(), annot=True , cmap='coolwarm')
+    plt.title("Correlation Heatmap")
 
 
 # load or prepare the classification dataset
@@ -82,25 +69,6 @@ def load_dataset():
 	Y = df[['CreditApprove',]].to_numpy()
 	return X, Y
 
-
-# Create a list with random entries of 1 or -1
-# list_length = 8
-# random_list = [random.choice([1, -1]) for _ in range(list_length)]
-
-# print(random_list)
-
-
-
-# example of hill climbing the test set for a classification task
-from random import randint
-from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from matplotlib import pyplot
-import random
-# load or prepare the classification dataset
-# def load_dataset():
-# 	return make_classification(n_samples=5000, n_features=20, n_informative=15, n_redundant=5, random_state=1)
 
 
 # load or prepare the classification dataset
@@ -205,23 +173,32 @@ def hill_climb_testset(sample_x, sample_y, max_iterations):
 
     return best_weights, errors, values
 
-# load the dataset
-X, y = load_dataset()
-print(X.shape, y.shape)
-# split dataset into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
-print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
-# run hill climb
-# yhat, scores = hill_climb_testset(X_test, y_test, 20000)
-solution_weights, errors, values = hill_climb_testset(X_train, y_train, 100)
-# plot the scores vs iterations
-# pyplot.plot(errors)
-plt.figure(figsize = (10,6))
-
-plt.plot(errors, label='Errors 1', color='blue', marker='o')  # First dataset
-plt.plot(values, label='Errors 2', color='red', marker='x')   # Second dataset
 
 
-print(f"The train error for weights: {solution_weights} is {loss_function(X_train, y_train, solution_weights)}")
-print(f"The test error for weights: {solution_weights} is {loss_function(X_test, y_test, solution_weights)}")
-pyplot.show()
+if __name__ == "__main__":
+    # load the dataset
+    X, Y = load_dataset()
+    print(X.shape, Y.shape)
+    # split dataset into train and test sets
+    # X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
+    # print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+    # run hill climb
+    optimal_weight, errors, values = hill_climb_testset(X, Y, 100)
+    best_error_optimal_weight = round(loss_function(X, Y, optimal_weight),3)
+    # feature_corrlelation()
+    # column_distribution()
+
+    plt.figure(figsize = (10,6))
+    plt.text(x=1.4, y=0.8, s=f"Optimal weights: {optimal_weight}\nError er(w): {round(loss_function(X, Y, optimal_weight),3)}", fontsize=12, color='green')
+    plt.plot(values, label='Current Solution', color='red', marker='x')   # Second dataset
+    plt.plot(errors, label='Best Solutions', color='blue', marker='o')  # First dataset
+    # Labeling the axes
+    plt.xlabel('Round of Search')  # Replace with your desired x-axis label
+    plt.ylabel('Error of weights Er(w)')  # Replace with your desired y-axis label
+
+    # Adding a title and legend
+    plt.title('Figure 1 for Local Search')  # Optional: add a title
+    plt.legend()
+    print(f"For whole dataset, the error for weights: {optimal_weight} is {best_error_optimal_weight}")
+    # print(f"The error for weights: {optimal_weight} is {loss_function(X_test, y_test, optimal_weight)}")
+    pyplot.show()
